0cbe37d9c9ecd7ad46ec1d5bf72ab0a9812b8e7a
Aidan Coyle <packrat386@gmail.com>
2024-04-27 23:33:56 -0500

    Add reading and writing index pages

--- disk_dal.go
+++ disk_dal.go
@@ -1,7 +1,10 @@
 package aggd
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
+	"fmt"
 	"os"
 
 	"github.com/google/uuid"
@@ -23,22 +26,64 @@ func (d *diskDAL) Read(ctx context.Context, keyPattern string, tr TimeRange) ([]
 
 type index struct {
 	f *os.File
+	l Locker
 }
 
 func (i *index) dataLocationFor(_ context.Context, path Path) (uuid.UUID, error) {
 	panic("not implemented")
 }
 
+func (i *index) readPage(ctx context.Context, n int64) (indexPage, error) {
+	// todo: rwlock?
+	i.l.Lock(ctx, "$index")
+	defer i.l.Unlock(ctx, "$index")
+
+	page := indexPage{}
+	buf := make([]byte, indexPageSize)
+
+	_, err := i.f.ReadAt(buf, n*indexPageSize)
+	if err != nil {
+		return page, fmt.Errorf("could not read page: %w", err)
+	}
+
+	err = binary.Read(bytes.NewBuffer(buf), binary.LittleEndian, &page)
+	if err != nil {
+		return page, fmt.Errorf("could not deserialize page information: %w", err)
+	}
+
+	return page, nil
+}
+
+func (i *index) writePage(ctx context.Context, n int64, page indexPage) error {
+	i.l.Lock(ctx, "$index")
+	defer i.l.Unlock(ctx, "$index")
+
+	buf := new(bytes.Buffer)
+
+	err := binary.Write(buf, binary.LittleEndian, page)
+	if err != nil {
+		return fmt.Errorf("could not serialize page information: %w", err)
+	}
+
+	_, err = i.f.WriteAt(buf.Bytes(), n*indexPageSize)
+	if err != nil {
+		return fmt.Errorf("could not write page: %w", err)
+	}
+
+	i.f.Sync()
+
+	return nil
+}
+
 const indexPageSize = 4096
 
 type indexPage struct {
-	dataLocation uuid.UUID
-	next         int64
-	padding      [32]byte
-	entries      [101]indexEntry
+	Next    int64
+	Entries [73]indexEntry
 }
 
 type indexEntry struct {
-	key  [32]byte
-	page int64
+	Key          [32]byte
+	IndexPage    int64
+	DataLocation uuid.UUID
 }
--- disk_dal_test.go
+++ disk_dal_test.go
@@ -1,17 +1,100 @@
 package aggd
 
 import (
+	"context"
 	"encoding/binary"
+	"os"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
+func padBytes(b []byte) [32]byte {
+	var ret [32]byte
+	copy(ret[:], b)
+
+	return ret
+}
+
 func TestIndexPageSize(t *testing.T) {
-	page := indexPage{
-		dataLocation: uuid.Must(uuid.NewRandom()),
-	}
+	page := indexPage{}
 
 	assert.Equal(t, indexPageSize, binary.Size(page))
+}
+
+func TestIndexPageReadWrite(t *testing.T) {
+	p0 := indexPage{
+		Next: 1,
+		Entries: [73]indexEntry{
+			{
+				Key:       padBytes([]byte("foo")),
+				IndexPage: 2,
+			},
+		},
+	}
+
+	p1 := indexPage{
+		Next: -1,
+		Entries: [73]indexEntry{
+			{
+				Key:       padBytes([]byte("baz")),
+				IndexPage: 3,
+			},
+		},
+	}
+
+	p2 := indexPage{
+		Next: -1,
+		Entries: [73]indexEntry{
+			{
+				Key:          padBytes([]byte("bar")),
+				DataLocation: uuid.Must(uuid.NewRandom()),
+			},
+		},
+	}
+
+	p3 := indexPage{
+		Next: -1,
+		Entries: [73]indexEntry{
+			{
+				Key:          padBytes([]byte("qux")),
+				DataLocation: uuid.Must(uuid.NewRandom()),
+			},
+		},
+	}
+
+	f, err := os.CreateTemp("", "index")
+	if err != nil {
+		t.Fatalf("could not create tempfile for index: %s", err.Error())
+	}
+	defer f.Close()
+
+	idx := &index{
+		f: f,
+		l: NewMemoryLocker(),
+	}
+
+	ctx := context.Background()
+
+	assert.NoError(t, idx.writePage(ctx, 3, p3))
+	assert.NoError(t, idx.writePage(ctx, 2, p2))
+	assert.NoError(t, idx.writePage(ctx, 1, p1))
+	assert.NoError(t, idx.writePage(ctx, 0, p0))
+
+	got0, err := idx.readPage(ctx, 0)
+	assert.NoError(t, err)
+	assert.Equal(t, p0, got0)
+
+	got1, err := idx.readPage(ctx, 1)
+	assert.NoError(t, err)
+	assert.Equal(t, p1, got1)
+
+	got2, err := idx.readPage(ctx, 2)
+	assert.NoError(t, err)
+	assert.Equal(t, p2, got2)
+
+	got3, err := idx.readPage(ctx, 3)
+	assert.NoError(t, err)
+	assert.Equal(t, p3, got3)
 }
