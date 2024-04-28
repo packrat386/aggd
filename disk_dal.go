46ae7ea5bd1499a28c17477f85daf61aa30c5307
Aidan Coyle <packrat386@gmail.com>
2024-04-28 08:30:00 -0500

    Add the ability to find a data location

--- disk_dal.go
+++ disk_dal.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"os"
 
@@ -29,16 +30,82 @@ type index struct {
 	l Locker
 }
 
-func (i *index) dataLocationFor(_ context.Context, path Path) (uuid.UUID, error) {
+func (i *index) createDataLocationFor(ctx context.Context, path path, u uuid.UUID) error {
 	panic("not implemented")
 }
 
-func (i *index) readPage(ctx context.Context, n int64) (indexPage, error) {
+var errNotFound = errors.New("data location not found")
+
+func (i *index) dataLocationFor(ctx context.Context, path Path) (uuid.UUID, error) {
+	var (
+		currentPage      *indexPage
+		currentComponent []byte
+		remaining        Path
+		ctr              int64
+		err              error
+	)
+
+	currentPage, err = i.readPage(ctx, 0)
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("could not read root page: %w", err)
+	}
+
+	currentComponent = path[0]
+	remaining = path[1:]
+
+	for {
+		// sanity check
+		ctr += 1
+		if ctr > 1000 {
+			return uuid.UUID{}, fmt.Errorf("search too deep")
+		}
+
+		entry, found := currentPage.entryFor(currentComponent)
+		if found {
+			if len(remaining) == 0 { // we're done
+				fmt.Println(1)
+				if (entry.DataLocation == uuid.UUID{}) {
+					return uuid.UUID{}, errNotFound
+				}
+
+				return entry.DataLocation, nil
+			} else if entry.IndexPage == 0 { // this entry doesn't have anything under it
+				fmt.Println(2)
+				return uuid.UUID{}, errNotFound
+			} else { // search next component in path
+				fmt.Println(3)
+				currentPage, err = i.readPage(ctx, entry.IndexPage)
+				if err != nil {
+					return uuid.UUID{}, fmt.Errorf("could not read page [%d]: %w", entry.IndexPage, err)
+				}
+
+				currentComponent = remaining[0]
+				remaining = remaining[1:]
+				continue
+			}
+		} else {
+			if currentPage.Next != 0 { // search next page for this component
+				fmt.Println(5)
+				currentPage, err = i.readPage(ctx, currentPage.Next)
+				if err != nil {
+					return uuid.UUID{}, fmt.Errorf("could not read page [%d]: %w", entry.IndexPage, err)
+				}
+
+				continue
+			} else { // no next page and not on this page
+				fmt.Println(6)
+				return uuid.UUID{}, errNotFound
+			}
+		}
+	}
+}
+
+func (i *index) readPage(ctx context.Context, n int64) (*indexPage, error) {
 	// todo: rwlock?
 	i.l.Lock(ctx, "$index")
 	defer i.l.Unlock(ctx, "$index")
 
-	page := indexPage{}
+	page := new(indexPage)
 	buf := make([]byte, indexPageSize)
 
 	_, err := i.f.ReadAt(buf, n*indexPageSize)
@@ -46,7 +113,7 @@ func (i *index) readPage(ctx context.Context, n int64) (indexPage, error) {
 		return page, fmt.Errorf("could not read page: %w", err)
 	}
 
-	err = binary.Read(bytes.NewBuffer(buf), binary.LittleEndian, &page)
+	err = binary.Read(bytes.NewBuffer(buf), binary.LittleEndian, page)
 	if err != nil {
 		return page, fmt.Errorf("could not deserialize page information: %w", err)
 	}
@@ -54,13 +121,13 @@ func (i *index) readPage(ctx context.Context, n int64) (indexPage, error) {
 	return page, nil
 }
 
-func (i *index) writePage(ctx context.Context, n int64, page indexPage) error {
+func (i *index) writePage(ctx context.Context, n int64, page *indexPage) error {
 	i.l.Lock(ctx, "$index")
 	defer i.l.Unlock(ctx, "$index")
 
 	buf := new(bytes.Buffer)
 
-	err := binary.Write(buf, binary.LittleEndian, page)
+	err := binary.Write(buf, binary.LittleEndian, *page)
 	if err != nil {
 		return fmt.Errorf("could not serialize page information: %w", err)
 	}
@@ -86,4 +153,43 @@ type indexEntry struct {
 	Key          [32]byte
 	IndexPage    int64
 	DataLocation uuid.UUID
+}
+
+func (i *indexPage) entryFor(key []byte) (indexEntry, bool) {
+	padded := padBytes(key)
+
+	for _, e := range i.Entries {
+		if padded == e.Key {
+			return e, true
+		}
+	}
+
+	return indexEntry{}, false
+}
+
+func (i *indexPage) addIndexPageFor(key []byte, n int64) {
+	padded := padBytes(key)
+
+	for idx, e := range i.Entries {
+		if padded == e.Key {
+			i.Entries[idx].IndexPage = n
+		}
+	}
+}
+
+func (i *indexPage) addDataLocationFor(key []byte, u uuid.UUID) {
+	padded := padBytes(key)
+
+	for idx, e := range i.Entries {
+		if padded == e.Key {
+			i.Entries[idx].DataLocation = u
+		}
+	}
+}
+
+func padBytes(b []byte) [32]byte {
+	var ret [32]byte
+	copy(ret[:], b)
+
+	return ret
 }
--- disk_dal_test.go
+++ disk_dal_test.go
@@ -10,13 +10,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func padBytes(b []byte) [32]byte {
-	var ret [32]byte
-	copy(ret[:], b)
-
-	return ret
-}
-
 func TestIndexPageSize(t *testing.T) {
 	page := indexPage{}
 
@@ -24,7 +17,7 @@ func TestIndexPageSize(t *testing.T) {
 }
 
 func TestIndexPageReadWrite(t *testing.T) {
-	p0 := indexPage{
+	p0 := &indexPage{
 		Next: 1,
 		Entries: [73]indexEntry{
 			{
@@ -34,8 +27,8 @@ func TestIndexPageReadWrite(t *testing.T) {
 		},
 	}
 
-	p1 := indexPage{
-		Next: -1,
+	p1 := &indexPage{
+		Next: 0,
 		Entries: [73]indexEntry{
 			{
 				Key:       padBytes([]byte("baz")),
@@ -44,8 +37,8 @@ func TestIndexPageReadWrite(t *testing.T) {
 		},
 	}
 
-	p2 := indexPage{
-		Next: -1,
+	p2 := &indexPage{
+		Next: 0,
 		Entries: [73]indexEntry{
 			{
 				Key:          padBytes([]byte("bar")),
@@ -54,8 +47,8 @@ func TestIndexPageReadWrite(t *testing.T) {
 		},
 	}
 
-	p3 := indexPage{
-		Next: -1,
+	p3 := &indexPage{
+		Next: 0,
 		Entries: [73]indexEntry{
 			{
 				Key:          padBytes([]byte("qux")),
@@ -97,4 +90,92 @@ func TestIndexPageReadWrite(t *testing.T) {
 	got3, err := idx.readPage(ctx, 3)
 	assert.NoError(t, err)
 	assert.Equal(t, p3, got3)
+}
+
+func TestDataLocationFor(t *testing.T) {
+	loc1 := uuid.Must(uuid.NewRandom())
+	loc2 := uuid.Must(uuid.NewRandom())
+
+	p0 := &indexPage{
+		Next: 1,
+		Entries: [73]indexEntry{
+			{
+				Key:       padBytes([]byte("foo")),
+				IndexPage: 2,
+			},
+		},
+	}
+
+	p1 := &indexPage{
+		Next: 0,
+		Entries: [73]indexEntry{
+			{
+				Key:       padBytes([]byte("baz")),
+				IndexPage: 3,
+			},
+		},
+	}
+
+	p2 := &indexPage{
+		Next: 0,
+		Entries: [73]indexEntry{
+			{
+				Key:          padBytes([]byte("bar")),
+				DataLocation: loc1,
+			},
+		},
+	}
+
+	p3 := &indexPage{
+		Next: 0,
+		Entries: [73]indexEntry{
+			{
+				Key:          padBytes([]byte("qux")),
+				DataLocation: loc2,
+			},
+		},
+	}
+
+	f, err := os.CreateTemp("", "index")
+	if err != nil {
+		t.Fatalf("could not create tempfile for index: %s", err.Error())
+	}
+	defer f.Close()
+
+	idx := &index{
+		f: f,
+		l: NewMemoryLocker(),
+	}
+
+	ctx := context.Background()
+
+	assert.NoError(t, idx.writePage(ctx, 3, p3))
+	assert.NoError(t, idx.writePage(ctx, 2, p2))
+	assert.NoError(t, idx.writePage(ctx, 1, p1))
+	assert.NoError(t, idx.writePage(ctx, 0, p0))
+
+	got1, err := idx.dataLocationFor(ctx, mustPath("foo.bar"))
+	assert.NoError(t, err)
+	assert.Equal(t, loc1, got1)
+
+	got2, err := idx.dataLocationFor(ctx, mustPath("baz.qux"))
+	assert.NoError(t, err)
+	assert.Equal(t, loc2, got2)
+
+	got3, err := idx.dataLocationFor(ctx, mustPath("not.even.close"))
+	assert.Zero(t, got3)
+	assert.ErrorIs(t, err, errNotFound)
+
+	got4, err := idx.dataLocationFor(ctx, mustPath("foo.baz"))
+	assert.Zero(t, got4)
+	assert.ErrorIs(t, err, errNotFound)
+}
+
+func mustPath(s string) Path {
+	p, err := ParsePath(s)
+	if err != nil {
+		panic(err)
+	}
+
+	return p
 }
